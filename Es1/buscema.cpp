b1ec95e6ebc2488229f1dbef70f4f7a777ef85e5
Alessandro Porpiglia <114176944+alex-211@users.noreply.github.com>
2023-07-31 19:29:01 +0200

    Da finire

---
+++ Es1/buscema.cpp
@@ -0,0 +1,43 @@
+/*Autore: Stefano Buscema; Classe: 2DS; Data: 31/07/23; Testo;*/
+
+#include <iostream>
+
+int main() {
+    int n, input, maggiore, minore, differenza, totale, media;
+    std::cout<<"Quanti numeri vuoi inserire?";
+    std::cin>>n;
+    
+    int i=0;
+    // inzia il ciclo, ad ogni ripetizione la variabile input viene sovrascritta
+    for (i<n, i++)
+    {
+        cin>>input;
+        // la prima volta che il ciclo gira le variabili maggiore e minore vengono popolate col primo input così da non dover usare una valore arbitrario
+        if (i==0)
+        {
+            maggiore = input;
+            minore = input;
+        }
+        
+        if (input > maggiore)
+        {
+            maggiore = input;
+        }
+        
+        if (input < minore)
+        {
+            minore = input;
+        }
+        
+        totale += input;
+    }
+    media = totale/n;
+    differenza = maggiore-minore;
+    
+    cout>>"Il numero maggiore è: " + maggiore;
+    cout>>"Il numero minore è: " + minore;
+    cout>>"La differenza tra i due è: " + differenza;
+    cout>>"La media tra i due è: " + media;
+    
+    return;
+}
