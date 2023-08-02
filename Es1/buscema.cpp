759f5526967556e5efd26d98ad95b7d0f7e4c791
Alessandro Porpiglia <114176944+alex-211@users.noreply.github.com>
2023-08-02 19:46:10 +0200

    Finito es. 1
    Corretto un erroraccio del cazzo, mannaggia a me. E manco GPT l'ha azzeccato al primo colpo

--- Es1/buscema.cpp
+++ Es1/buscema.cpp
@@ -2,16 +2,16 @@
 #include <iostream>
 
 int main() {
-    int n, input, maggiore, minore, differenza, totale;
+    int n = 0, input = 0, maggiore = 0, minore = 0, differenza = 0, totale = 0 ;
     // media dichiarata come floating point var perchè da un valore più preciso
-    float media;
-    std::cout<<"Quanti numeri vuoi inserire?";
+    float media = 0;
+    std::cout<<"Quanti numeri vuoi inserire? ";
     std::cin>>n;
     
     // inzia il ciclo, ad ogni ripetizione la variabile input viene sovrascritta
-    for (int i = 0; i<n, i++;)
+    for (int i = 0; i<n; i++)
     {
-        std::cout<<"Inserisci un numero";
+        std::cout<<"Inserisci un numero: ";
         std::cin>>input;
         // la prima volta che il ciclo gira le variabili maggiore e minore vengono popolate col primo input così da non dover usare una valore arbitrario
         if (i==0)
