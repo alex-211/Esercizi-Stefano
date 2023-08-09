450a25f94882cdab3fd1595dc05e76ea5b80ed8d
Alessandro Porpiglia <114176944+alex-211@users.noreply.github.com>
2023-08-09 12:34:57 +0200

    FInito es. 4

---
+++ Es4/buscema.cpp
@@ -0,0 +1,19 @@
+/*Autore: Stefano Buscema; Classe: 2Ds; Data: 9/8/23*/
+
+#include <iostream>
+
+int main() {
+    int input, maggiore;
+    maggiore = 0;
+
+    do {
+        std::cout<<"Inserisci un numero: ";
+        std::cin>>input;
+        if (input > maggiore)
+        {
+            maggiore = input;
+        }
+    }
+    while (input != 0);
+    std::cout<<"Il numero maggiore è: " + maggiore;
+}
